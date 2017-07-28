c8368aab234fb102f23fbbb93b250ba57edddaad
April Chen <achen@civisanalytics.com>
2017-07-27 21:54:37 -0500

    Fix join issues caused by npi duplicates

--- etl/make_mips_data.sql
+++ etl/make_mips_data.sql
@@ -2,29 +2,32 @@
 SET SEED TO 0.25;
 
 --Create raw scores for MIPS performance categories
+DROP TABLE IF EXISTS public.cms_mips_scores;
+
 CREATE TABLE public.cms_mips_scores
-DISTKEY(npi)
-SORTKEY(npi) AS (
+DISTKEY(id)
+SORTKEY(id) AS (
   SELECT
-    npi
-    ,prvdr_org_name
+    id
     ,CAST(RANDOM() * 70 AS INT) AS quality
     ,CAST(RANDOM() * 100 AS INT) AS cost
     ,CAST(RANDOM() * 40 AS INT) AS improvement_activities
     ,CAST(RANDOM() * 155 AS INT) AS advancing_care_info
-  FROM public.cms_eligibility
+  FROM public.cms_eligibility_id
   WHERE individual_low_vol_stus_rsn_cd IS NULL
   );
 
 --Append MIPS scores back to provider eligibility data
+DROP TABLE IF EXISTS public.cms_eligibility_mips;
+
 CREATE TABLE public.cms_eligibility_mips
 DISTKEY(npi)
 SORTKEY(npi) AS (
   SELECT
   *
-  FROM public.cms_eligibility
+  FROM public.cms_eligibility_id
   LEFT JOIN public.cms_mips_scores
-  USING (npi, prvdr_org_name)
+  USING (id)
   WHERE individual_low_vol_stus_rsn_cd IS NULL
 );
 
